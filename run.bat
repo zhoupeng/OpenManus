62f937c1ca8fb5da09d1f07a90c0f68476b3c547
Feige-cn <boyfegie@163.com>
2025-03-11 13:48:00 +0800

    Synchronize built-in environment

--- run.bat
+++ run.bat
@@ -1,3 +1,16 @@
 @echo off
-venv\Scripts\python.exe app.py
+setlocal
+cd /d %~dp0
+
+call venv\Scripts\activate.bat
+
+echo Trying to sync with GitHub repository...
+git pull origin front-end || (
+    echo Failed to sync with GitHub, skipping update...
+)
+
+echo Starting Python application...
+python app.py
+
 pause
+endlocal
